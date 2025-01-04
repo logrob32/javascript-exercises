59b4c74b1acff8cf9d7c83a26bfe4738f51fe417
Logan Roberts <lroberts@marketingmilk.com>
2025-01-04 16:35:12 -0600

    solves first 7 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let reverseWord = "";
+  for (i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  return reverseWord;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    Number.isInteger(num1) &&
+    Number.isInteger(num2) &&
+    num1 >= 0 &&
+    num2 >= 0
+  ) {
+    let topNumber = num1;
+    let bottomNumber = num2;
+    if (num1 <= num2) {
+      topNumber = num2;
+      bottomNumber = num1;
+    }
+    let total = 0;
+    for (let i = bottomNumber; i <= topNumber; i++) {
+      total += i;
+    }
+    return total;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
